f99962218bfcdbf3aee346809bd1d910f5ec8793
DaTan99 <tand2@myumanitoba.ca>
2023-02-02 22:57:39 -0600

    Add comment schema
    This commit adds the schema for Comment

---
+++ backend/schema/comment-schema.js
@@ -0,0 +1,29 @@
+const mongoose =  require('mongoose')
+const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+
+    post_id: { //the id of the post for the comment
+        type: String, // mongoose.ObjectId,
+        required: true
+    },
+
+    user_id: { //the id of the user who posted the comment
+        type: String, // mongoose.ObjectId,
+        required: true
+    },
+
+    content: {
+        type: String,
+        required: true,
+        default: ""
+    },
+
+    comment_date: { //the date the comment was created
+        type: Date,
+        required: true,
+        default: Date.now()
+    },
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
