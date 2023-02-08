dc8c080b04ee965451d73879c278b646d18976f0
Dean Cruz <cruzd@myumanitoba.ca>
2023-02-07 20:50:18 -0600

    Write test suite for Like model and service

--- backend/model/likes-model.js
+++ backend/model/likes-model.js
@@ -7,12 +7,20 @@ const getNumLikes = async (post_id) => {
 
 //determine if the user has liked a post
 const userLikedPost = async (post_id, user_id) => {
-    return await (Like.countDocuments({post_id, user_id}) > 0);
+    return await Like.countDocuments({post_id, user_id}) > 0;
 }
 
 //like a post
 const likePost = async (post_id, user_id) => {
-    return await Like.create({post_id, user_id});
+    const hasLiked = await userLikedPost(post_id, user_id);
+    let returnValue;
+
+    //don't want to like the post if the user has already liked it
+    if (!hasLiked) {
+        returnValue = await Like.create({post_id, user_id});
+    }
+
+    return returnValue;
 }
 
 //unlike a post
