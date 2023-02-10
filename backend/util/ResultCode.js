d459a3cc21e8dcd5f34399545a5d8d8b5defcc42
weiyu <weiyu0203@qq.com>
2023-02-10 01:02:36 -0600

    some bugs fixed, rebuild route structure, create a new Result type

--- backend/util/ResultCode.js
+++ backend/util/ResultCode.js
@@ -10,6 +10,7 @@ class ResultCode{
     static SUCCESS = new ResultCode(20000, 'success');
     static UPDATE_FAIL = new ResultCode(40001, 'Fail to update information in database')
     static INVALID_USERID = new ResultCode(40002, 'The given user_id is invalid')
+    static INVALID_POSTID = new ResultCode(40003, 'The given post_id is invalid')
     static FAIL = new ResultCode(40000, 'fail');
 }
 
