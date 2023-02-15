6fa2c8a39be5d792b3d7a4b898551caba49ffa77
Dean Cruz <cruzd@myumanitoba.ca>
2023-02-14 21:29:23 -0600

    Change test to be unit tests and not integration

--- backend/tests/like-service-test.js
+++ backend/tests/like-service-test.js
@@ -5,7 +5,7 @@ const services = require('../service/likes-service');
 const expect = require('chai').expect;
 require("dotenv").config();
 
-const useRealDatabase = true;
+const useRealDatabase = false;
 
 /* generateLikes
  *
