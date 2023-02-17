03925f260a0a2a4478c5ec12b2d62f45dccf9189
WeiyuSun <98439184+WeiyuSun@users.noreply.github.com>
2023-02-17 11:08:36 -0600

    Update free-route.js
    typing error fixed

--- backend/route/free-route.js
+++ backend/route/free-route.js
@@ -4,6 +4,6 @@ const userMiddleware = require("../middleware/user-middleware")
 
 // add all routes which don't need authentication check in this file
 router.post("/user/login", [userMiddleware.emailValidation, userMiddleware.passwordValidation], userController.login)
-router.post("/user/signup", userMiddleware.all, userController.singup)
+router.post("/user/signup", userMiddleware.all, userController.signup)
 
 module.exports = router
