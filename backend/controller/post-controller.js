f08a2ba37442aeaad88df2db76cdb254d7a29f18
DaTan99 <tand2@myumanitoba.ca>
2023-03-15 17:14:58 -0500

    format post-controller

--- backend/controller/post-controller.js
+++ backend/controller/post-controller.js
@@ -120,50 +120,56 @@ async function getPostsInfo(posts) {
         numberLikes: likes,
       });
     }
+  }
 }
 
 //get posts from subscribed channels
 const getSubscribedPosts = async (req, res) => {
-    const { user_id, start, size } = req.query
+    const { user_id, start, size } = req.query;
 
     if(!mongoose.Types.ObjectId.isValid(user_id)){
-        return res.json(Result.invalidUserId())
+        return res.json(Result.invalidUserId());
     }
 
     try{
-        const subscribes = await subscribeService.getUserFollowingPage(user_id, start, size)
-        const creators = [...new Set(subscribes.map( subscribe => subscribe.creator_id ))]
-        let all_posts = []
-        let all_result = []
+        const subscribes = await subscribeService.getUserFollowingPage(user_id, start, size);
+        const creators = [...new Set(subscribes.map( subscribe => subscribe.creator_id ))];
+        let all_posts = [];
+        let all_result = [];
 
         for(let creator of creators){
-            const posts = await postService.getAllPostsFromUser(creator)
-            const result = await getPostsInfo(posts)   
-            all_posts.extend(posts)
-            all_result.extend(result)
+            const posts = await postService.getAllPostsFromUser(creator);
+            const result = await getPostsInfo(posts);
+            all_posts.extend(posts);
+            all_result.extend(result);
         }
         
-        res.json(Result.success(all_result))
+        res.json(Result.success(all_result));
     }catch(err){
-        res.json(Result.fail(err))
+        res.json(Result.fail(err));
     }
 }
 
 Array.prototype.extend = function (array) {
-    array.forEach(item => this.push(item))
+    array.forEach(item => this.push(item));
   }
 
 async function getPostsInfo(posts){
-    let result = []
+    let result = [];
     for( let post of posts){
-        const user = await userService.getUserInfo(post.user_id)
-        const likes = await likeService.getNumLikes(post._id)
+        const user = await userService.getUserInfo(post.user_id);
+        const likes = await likeService.getNumLikes(post._id);
         if( user !== null ){
-            result.push({post:post, username: user.username, affiliation: user.affiliation, 
-                profile_photo: user.profile_photo, numberLikes: likes})
+            result.push({
+                post:post, username: 
+                user.username, affiliation: 
+                user.affiliation, 
+                profile_photo: user.profile_photo, 
+                numberLikes: likes
+            });
         }
     }
-    return result
+    return result;
 }
 
 module.exports = {
@@ -173,5 +179,5 @@ module.exports = {
     removePostByID, 
     getRecentPost, 
     getAllPostsFromUser,
-    getSubscribedPosts
+    getSubscribedPosts,
 }
