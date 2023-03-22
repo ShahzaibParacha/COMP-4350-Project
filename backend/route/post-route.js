52e2569c2434917d851a4f9769d1101117520455
DaTan99 <tand2@myumanitoba.ca>
2023-03-22 15:10:30 -0500

    change route format

--- backend/route/post-route.js
+++ backend/route/post-route.js
@@ -4,9 +4,9 @@ const router = require('express').Router();
 router.post('/create', postController.createPost);
 router.post('/update', postController.updatePostContent);
 router.delete('/update', postController.removePostByID);
-router.get('/get_post_by_ID', postController.getPostByID);
-router.get('/get_recent_posts', postController.getRecentPost);
-router.get('/get_user_posts', postController.getAllPostsFromUser);
-router.get('/get_subscribed_posts', postController.getSubscribedPosts);
+router.get('/getPostByID', postController.getPostByID);
+router.get('/getRecentPosts', postController.getRecentPost);
+router.get('/getUserPosts', postController.getAllPostsFromUser);
+router.get('/getSubscribedPosts', postController.getSubscribedPosts);
 
 module.exports = router;
